c4bd4aa29c42e72573d1007525df23ae36bb8293
puiuld <puiuld47@gmail.com>
2025-02-19 09:41:17 +0200

    Add files via upload

---
+++ -26/-26/ConsoleApp1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] omas=Input();
+
+            Output(omas);
+            Console.ReadLine();
+        }
+        static int[] Input()
+        {
+            Console.Write("n=");
+            int n = int.Parse(Console.ReadLine());
+            int[] omas = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write($"omas[{i}]=");
+                omas[i] = int.Parse(Console.ReadLine());
+            }
+            return omas;
+
+        }
+        
+        static void Output(int[] omas) 
+        { 
+        
+            for (int i = 0; i < omas.Length; i++)
+            {
+                Console.WriteLine($"omas[{i}]={omas[i]}");
+            }
+        }
+    
+    }
+}
---
+++ -26/ConsoleApp1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] omas = Input();
+            Output(omas);
+            Console.ReadLine();
+        }
+        static int[] Input()
+        {
+            Console.Write("n=");
+            int n = int.Parse(Console.ReadLine());
+            int[] omas = new int[n];
+            Random unt = new Random();
+            for (int i = 0; i < omas.Length; i++)
+            {
+                omas[i] = unt.Next(-10, 10);
+            }
+            return omas;
+            
+        }
+        static void Output(int[] omas)
+        {
+
+            for (int i = 0; i < omas.Length; i++)
+            {
+                Console.WriteLine($"omas[{i}]={omas[i]}");
+            }
+        }
+        
+
+    }
+}
